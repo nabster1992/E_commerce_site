9bae625600730dbda5584baa6aad0edada93d845
Nab <nhussainjee@gmail.com>
2023-09-10 02:32:57 +0500

    added cart view  feature with add/remove capabilitites

--- store/cart.py
+++ store/cart.py
@@ -5,3 +5,56 @@ from .models import Product
 class Cart(object):
     def __init__(self, request):
         self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+
+        self.cart = cart
+
+    def __iter__(self):
+        for p in self.cart.keys():
+            self.cart[str(p)]['product'] = Product.objects.get(pk=p)
+
+        for item in self.cart.values():
+            item['total_price'] = int(item['product'].price * item['quantity']) / 100
+
+            yield item
+    
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+    
+    def save(self):
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
+
+    def add(self, product_id, quantity=1, update_quantity=False):
+        product_id = str(product_id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 1, 'id': product_id}
+
+        if update_quantity:
+            self.cart[product_id] += int(quantity)
+        
+        self.save()
+
+    def get_total_cost(self): 
+         for p in self.cart.keys():
+             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
+
+
+         return int(sum(item['product'].price * item['quantity'] for item in self.cart.values())) /200
+            #we sum together the price of product multiplied by quantity of
+            # each items within the cart divided by 100, we then convert so its an integer
+        
+    def remove(self, product_id, update_quantity=True):
+        if product_id in self.cart:
+            del self.cart[product_id]
+        
+            self.save()
+
+        
+            
+
+    
