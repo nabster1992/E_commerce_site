7b43091aaccdf5da11182d92f0d81bd164bc62b0
Nab <nhussainjee@gmail.com>
2023-09-13 06:56:00 +0500

    changed website url for success and fail within view, stored in settings

--- store/views.py
+++ store/views.py
@@ -62,8 +62,8 @@ def checkout(request):
             payment_method_types=['card'],
             line_items=items,
             mode='payment',
-            success_url = 'http://127.0.0.1:8000/cart/success',
-            cancel_url =  'http://127.0.0.1:8000/cart/', 
+            success_url = f'{settings.WEBSITE_URL}cart/success/',
+            cancel_url =  f'{settings.WEBSITE_URL}cart/',
         )
 
         payment_intent = stripe.PaymentIntent.create(
